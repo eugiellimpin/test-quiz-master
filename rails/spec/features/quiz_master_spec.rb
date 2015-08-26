bbbcb94309209a0dda5c63d253601c3121f5c85b
Eugie Limpin <eugiellimpin@gmail.com>
2015-08-26 19:22:24 +0800

    Test create question fail scenarios
    - Test scenario where 'Question' field is left blank
    - Test scenario where 'Answer' field is left blank
    
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- rails/spec/features/quiz_master_spec.rb
+++ rails/spec/features/quiz_master_spec.rb
@@ -11,4 +11,24 @@ RSpec.feature 'Quiz Master', :type => :feature do
 
     expect(page).to have_content 'Question created successfully'
   end
+
+  scenario 'User leaves Question field blank' do
+    visit '/questions/new'
+
+    fill_in 'Answer', :with => 'Answer without a question'
+
+    click_button 'Create Question'
+
+    expect(page).to have_content "Question can't be blank"
+  end
+
+  scenario 'User leaves Answer field blank' do
+    visit '/questions/new'
+
+    fill_in 'Question', :with => 'What is the meaning of life?'
+
+    click_button 'Create Question'
+
+    expect(page).to have_content "Answer can't be blank"
+  end
 end
