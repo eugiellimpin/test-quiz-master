1cd86068df85de2cb685b8d3857ab24a7eaa7ab4
Eugie Limpin <eugiellimpin@gmail.com>
2015-08-26 18:23:24 +0800

    Test GET questions_controller#index
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- rails/spec/controllers/questions_controller_spec.rb
+++ rails/spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, :type => :controller do
-  it 'is missing specs'
+  describe "Get #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "load all of the questions into @questions" do
+      question1 = Question.create(question: "Foo?", answer: "Bar")
+      question2 = Question.create(question: "Bar?", answer: "Baz")
+
+      get :index
+      expect(assigns(:questions)).to match_array([question1, question2])
+    end
+  end
 end
