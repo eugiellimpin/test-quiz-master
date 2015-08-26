8fbfcba0055cc7abd83f2baeae23985962ce7ae4
Eugie Limpin <eugiellimpin@gmail.com>
2015-08-26 20:05:40 +0800

    Test update question fail scenarios
    - Test scenario where 'Question' field is left blank
    - Test scenario where 'Answer' field is left blank
    - Move call to visit to before block to remove duplication
    
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- rails/spec/features/user_updates_a_question_spec.rb
+++ rails/spec/features/user_updates_a_question_spec.rb
@@ -4,16 +4,30 @@ RSpec.feature 'Update question', :type => :feature do
   before :each do
     @question = Question.create(:question => 'What is the meaning of life?', :answer => "42")
     @question.save
+    visit(edit_question_path(@question))
   end
 
   scenario 'user updates a question' do
-    visit(edit_question_path(@question))
-
     fill_in 'Question', :with => 'Are you sure that is the meaning of life?'
     fill_in 'Answer', :with => 'Yes'
-
     click_button 'Update Question'
 
     expect(page).to have_content "Question saved successfully"
+  end
+
+  scenario 'user leaves Question field blank' do
+    fill_in 'Question', :with => ''
+    fill_in 'Answer', :with => 'Answer without a question'
+    click_button 'Update Question'
+
+    expect(page).to have_content "Question can't be blank"
+  end
+
+  scenario 'user leaves Answer field blank' do
+    fill_in 'Question', :with => 'What is the meaning of life?'
+    fill_in 'Answer', :with => ''
+    click_button 'Update Question'
+
+    expect(page).to have_content "Answer can't be blank"
   end
 end
