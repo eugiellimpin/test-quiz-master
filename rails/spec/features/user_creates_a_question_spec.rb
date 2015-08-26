eb9d21565d15378a965280d4e7ecdf5d885275c9
Eugie Limpin <eugiellimpin@gmail.com>
2015-08-26 20:01:14 +0800

    Create separate spec files for each feature
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- rails/spec/features/user_creates_a_question_spec.rb
+++ rails/spec/features/user_creates_a_question_spec.rb
@@ -33,20 +33,3 @@ RSpec.feature 'Quiz Master', :type => :feature do
   end
 end
 
-RSpec.feature 'Update question', :type => :feature do
-  before :each do
-    @question = Question.create(:question => 'What is the meaning of life?', :answer => "42")
-    @question.save
-  end
-
-  scenario 'user updates a question' do
-    visit(edit_question_path(@question))
-
-    fill_in 'Question', :with => 'Are you sure that is the meaning of life?'
-    fill_in 'Answer', :with => 'Yes'
-
-    click_button 'Update Question'
-
-    expect(page).to have_content "Question saved successfully"
-  end
-end
