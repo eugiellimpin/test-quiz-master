68cd6f562dbccfe7e57f6af3624e5a6751da7648
Eugie Limpin <eugiellimpin@gmail.com>
2015-08-26 18:00:42 +0800

    Update is_correct? to understand numbers as words
    Use to_i method of String to convert the answer to an integer but only
    if it matches the pattern /^-?\d+$/. This way, we only convert strings
    to a number if it has a valid number format (one or more digits
    following an optional negative sign). After converting the string to a
    number, use humanize method to convert the number to its equivalent
    english word.
    
    Signed-off-by: Eugie Limpin <eugiellimpin@gmail.com>

--- rails/app/models/question.rb
+++ rails/app/models/question.rb
@@ -4,7 +4,15 @@ class Question < ActiveRecord::Base
   validates_presence_of :answer
 
   def is_correct?(submission)
-    answer.squish.downcase == submission.squish.downcase
+    formatted_answer = answer.squish.downcase
+    formatted_submission = submission.squish.downcase
+    number_to_string(formatted_answer) == number_to_string(formatted_submission)
+  end
+
+  private
+
+  def number_to_string(string)
+    string.match(/^-?\d+$/) ? string.to_i.humanize : string
   end
 
 end
